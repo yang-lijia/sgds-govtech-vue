c6ef406237eab66b1956db7e14efeeba1328f778
Wei Junyu <weijunyu@users.noreply.github.com>
2019-08-06 00:33:25 +0800

    Updated example dropdown code

--- src/docs/Dropdown.vue
+++ src/docs/Dropdown.vue
@@ -72,18 +72,30 @@ export default {
         return {
             codeSample: `<sgds-dropdown text="Click me">
     <sgds-dropdown-item>Item 1</sgds-dropdown-item>
-    <sgds-dropdown-item>Item 2</sgds-dropdown-item>
-    <sgds-dropdown-item>Item 3</sgds-dropdown-item>
+    <sgds-dropdown-item href="#!">Item 2, a link</sgds-dropdown-item>
     <sgds-dropdown-divider></sgds-dropdown-divider>
-    <sgds-dropdown-item>Item 4</sgds-dropdown-item>
+    <sgds-dropdown-item>
+        <p>
+            You can slot
+            <strong>anything</strong> you'd like here
+        </p>
+    </sgds-dropdown-item>
+    <sgds-dropdown-divider></sgds-dropdown-divider>
+    <sgds-dropdown-item>Item 3</sgds-dropdown-item>
 </sgds-dropdown>
 <sgds-dropdown text="Hover Me" hoverable>
     <sgds-dropdown-item>Item 1</sgds-dropdown-item>
-    <sgds-dropdown-item>Item 2</sgds-dropdown-item>
-    <sgds-dropdown-item>Item 3</sgds-dropdown-item>
+    <sgds-dropdown-item href="#!">Item 2, a link</sgds-dropdown-item>
     <sgds-dropdown-divider></sgds-dropdown-divider>
-    <sgds-dropdown-item>Item 4</sgds-dropdown-item>
-</sgds-dropdown>        
+    <sgds-dropdown-item>
+        <p>
+            You can slot
+            <strong>anything</strong> you'd like here
+        </p>
+    </sgds-dropdown-item>
+    <sgds-dropdown-divider></sgds-dropdown-divider>
+    <sgds-dropdown-item>Item 3</sgds-dropdown-item>
+</sgds-dropdown>
 `
         };
     }
